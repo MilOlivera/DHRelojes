551b2956add6fdf9a31d3d1e1dafafd5c89b026a
milolivera <mil_olivera@hotmail.com>
2022-04-06 14:38:06 -0300

    servidor y demas

---
+++ Proyecto/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+const publicPath = path.resolve(__dirname, "./public")
+
+app.use(express.static(publicPath));
+
+app.listen(3033, () => {
+    console.log("Servidor Corriendo")
+})
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+})
+app.get('/home', function(req, res){
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+    
+})
+
