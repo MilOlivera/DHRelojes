0c45d2cafbcad45872831b7a42a493e9643a01f0
valemirenzi <valeriamirenzi@gmail.com>
2022-08-21 20:00:38 -0300

    navbar

--- Proyecto/public/js/navbar.js
+++ Proyecto/public/js/navbar.js
@@ -1,5 +1,7 @@
 const primaryNav = document.querySelector('.primary-navigation')
 const navToggle = document.querySelector('.mobile-nav-toggle')
+const dropdown = document.querySelector('.dropdown-container')
+const dropdownLi = document.querySelector('.dropdownLi')
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
@@ -11,4 +13,13 @@ navToggle.addEventListener('click', () => {
         primaryNav.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
     }
+
+    // console.log(navToggle.ariaExpanded)
+    // if (navToggle.ariaExpanded  == 'true') {
+    //     body.classList.add('blur')
+    // }
 });
+
+dropdown.addEventListener('click', function() {
+    dropdownLi.classList.toggle('show')
+})
