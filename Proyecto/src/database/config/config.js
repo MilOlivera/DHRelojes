3ee9f0c2f6feb97b010d4c7a01f8cceda337b773
milolivera <mil_olivera@hotmail.com>
2023-09-26 12:38:55 -0300

    mm

--- Proyecto/src/database/config/config.js
+++ Proyecto/src/database/config/config.js
@@ -1,10 +1,14 @@
+const mysql2 = require('mysql2');
+
 module.exports = {
+  
   development: {
     username: "b4882f1d7b93de",
     password: "2364f73f",
     database: "heroku_1a6e1b66489a98f",
     host: "us-cdbr-east-06.cleardb.net",
     dialect: "mysql",
+    dialectModule: mysql2 // Needed to fix sequelize issues
   },
   test: {
     username: "b4882f1d7b93de",
@@ -12,6 +16,8 @@ module.exports = {
     database: "heroku_1a6e1b66489a98f",
     host: "us-cdbr-east-06.cleardb.net",
     dialect: "mysql",
+    dialectModule: mysql2 // Needed to fix sequelize issues
+
   },
   production: {
     username: "b4882f1d7b93de",
@@ -19,5 +25,7 @@ module.exports = {
     database: "heroku_1a6e1b66489a98f",
     host: "us-cdbr-east-06.cleardb.net",
     dialect: "mysql",
+    dialectModule: mysql2 // Needed to fix sequelize issues
+
   },
 };
